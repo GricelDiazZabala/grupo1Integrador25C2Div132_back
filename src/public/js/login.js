93b22eb680a49d8558e3ccaac7a30fea69d6b43b
Emiliano Nahuel Garcia <enahuelgarcia@gmail.com>
2025-11-24 01:07:10 -0300

    ADD : Añadido el iniciode la logica del login Todo : hacer el endpoint,controlador,model

---
+++ src/public/js/login.js
@@ -0,0 +1,45 @@
+let login_form = document.getElementById("login-form");
+import { API_BASE_URL } from "./config.js";
+
+login_form.addEventListener("submit", async (event) =>{
+    event.preventDefault();
+
+    let formData = new FormData(event.target);
+
+    let data = Object.fromEntries(formData.entries());
+
+    console.log(data);
+
+    enviarLogin(data);
+});
+
+async function enviarLogin(data) {
+
+
+    try {
+
+        let response = await fetch(API_BASE_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        let result = await response.json();
+
+        if(response.ok) {
+
+            console.log(result.message);
+            alert(result.message);
+
+            } else { 
+
+                console.error(result.message);
+                alert(result.message);
+
+        }
+        
+    } catch (error) {
+        console.error("Error: ", error);     
+    }}
